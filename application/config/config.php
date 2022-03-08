c217f38c95b58d0e3b3629701a862e754bfb8d96
David Dev <davidanderson04@live.com>
2022-03-08 18:49:17 -0300

    configuracao url base e database

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://localhost/sistema_aumox/';
 
 /*
 |--------------------------------------------------------------------------
@@ -326,7 +326,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/userguide3/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '$2a$12$RgBiaAS/87LAnEmvhH1YtO0a3YKtpsaaRlqyuhnifYhtIcFp4bopG';
 
 /*
 |--------------------------------------------------------------------------
